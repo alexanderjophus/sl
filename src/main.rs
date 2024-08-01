1b6b8f6328018d050db28118bbdbd364419446f4
AJT <alextrelore@gmail.com>
2024-08-01 20:16:10 +0100

    add -c flag for counting the number of carriages

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+mod train;
+
 use std::io::stdout;
 use std::{thread, time};
 
+use clap::{arg, command, Parser};
 use crossterm::terminal::{DisableLineWrap, EnableLineWrap};
 use crossterm::{
     cursor::{Hide, MoveTo, Show},
@@ -9,85 +12,41 @@ use crossterm::{
     terminal::{size, Clear, ClearType},
 };
 
-const LOCO_10: &str = "      ====        ________                ___________ ";
-const LOCO_11: &str = "  _D _|  |_______/        \\__I_I_____===__|_________| ";
-const LOCO_12: &str = "   |(_)---  |   H\\________/ |   |        =|___ ___|   ";
-const LOCO_13: &str = "   /     |  |   H  |  |     |   |         ||_| |_||   ";
-const LOCO_14: &str = "  |      |  |   H  |__--------------------| [___] |   ";
-const LOCO_15: &str = "  | ________|___H__/__|_____/[][]~\\_______|       |   ";
-const LOCO_16: &str = "  |/ |   |-----------I_____I [][] []  D   |=======|__ ";
+use train::{COAL_COLLECTION, LOCO_COLLECTION, WHEEL_COLLECTION};
 
-const LOCO_COLLECTION: [&str; 7] = [
-    LOCO_10, LOCO_11, LOCO_12, LOCO_13, LOCO_14, LOCO_15, LOCO_16,
-];
-
-const WHEEL_10: &str = "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ ";
-const WHEEL_11: &str = " |/-=|___|=    ||    ||    ||    |_____/~\\___/        ";
-const WHEEL_12: &str = "  \\_/      \\O=====O=====O=====O_/      \\_/            ";
-
-const WHEEL_20: &str = "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ ";
-const WHEEL_21: &str = " |/-=|___|=O=====O=====O=====O   |_____/~\\___/        ";
-const WHEEL_22: &str = "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            ";
-
-const WHEEL_30: &str = "__/ =| o |=-O=====O=====O=====O \\ ____Y___________|__ ";
-const WHEEL_31: &str = " |/-=|___|=    ||    ||    ||    |_____/~\\___/        ";
-const WHEEL_32: &str = "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            ";
-
-const WHEEL_40: &str = "__/ =| o |=-~O=====O=====O=====O\\ ____Y___________|__ ";
-const WHEEL_41: &str = " |/-=|___|=    ||    ||    ||    |_____/~\\___/        ";
-const WHEEL_42: &str = "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            ";
-
-const WHEEL_50: &str = "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ ";
-const WHEEL_51: &str = " |/-=|___|=   O=====O=====O=====O|_____/~\\___/        ";
-const WHEEL_52: &str = "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            ";
-
-const WHEEL_60: &str = "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ ";
-const WHEEL_61: &str = " |/-=|___|=    ||    ||    ||    |_____/~\\___/        ";
-const WHEEL_62: &str = "  \\_/      \\_O=====O=====O=====O/      \\_/            ";
-
-const WHEELS_10: [&str; 6] = [WHEEL_10, WHEEL_20, WHEEL_30, WHEEL_40, WHEEL_50, WHEEL_60];
-const WHEELS_11: [&str; 6] = [WHEEL_11, WHEEL_21, WHEEL_31, WHEEL_41, WHEEL_51, WHEEL_61];
-const WHEELS_12: [&str; 6] = [WHEEL_12, WHEEL_22, WHEEL_32, WHEEL_42, WHEEL_52, WHEEL_62];
-
-const WHEEL_COLLECTION: [[&str; 6]; 3] = [WHEELS_10, WHEELS_11, WHEELS_12];
-
-const COAL10: &str = "                              ";
-const COAL11: &str = "                              ";
-const COAL12: &str = "    _________________         ";
-const COAL13: &str = "   _|                \\_____A ";
-const COAL14: &str = " =|                        |  ";
-const COAL15: &str = " -|                        |  ";
-const COAL16: &str = "__|________________________|_ ";
-const COAL17: &str = "|__________________________|_ ";
-const COAL18: &str = "   |_D__D__D_|  |_D__D__D_|   ";
-const COAL19: &str = "    \\_/   \\_/    \\_/   \\_/";
-
-const COAL_COLLECTION: [&str; 10] = [
-    COAL10, COAL11, COAL12, COAL13, COAL14, COAL15, COAL16, COAL17, COAL18, COAL19,
-];
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Number of carriages to display
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+
     let (width, height) = size().expect("Unable to get terminal size");
     execute!(stdout(), Hide, DisableLineWrap).expect("Unable to hide cursor");
     for x in (0..width).rev() {
         execute!(stdout(), Clear(ClearType::All)).expect("Unable to clear screen");
         for i in 0..7 {
             execute!(stdout(), MoveTo(x, height - 10 + i)).expect("Unable to move cursor");
-            execute!(
-                stdout(),
-                Print(LOCO_COLLECTION[i as usize]),
-                Print(COAL_COLLECTION[i as usize]),
-            )
-            .expect("Unable to print");
+            execute!(stdout(), Print(LOCO_COLLECTION[i as usize]),).expect("Unable to print");
+            for _ in 0..args.count {
+                execute!(stdout(), Print(COAL_COLLECTION[i as usize]),).expect("Unable to print");
+            }
         }
         for i in 0..3 {
             execute!(stdout(), MoveTo(x, height - 3 + i)).expect("Unable to move cursor");
             execute!(
                 stdout(),
                 Print(WHEEL_COLLECTION[i as usize][(x % 3) as usize]),
-                Print(COAL_COLLECTION[(i + 7) as usize]),
             )
             .expect("Unable to print");
+            for _ in 0..args.count {
+                execute!(stdout(), Print(COAL_COLLECTION[(i + 7) as usize]))
+                    .expect("Unable to print");
+            }
         }
         thread::sleep(time::Duration::from_millis(60));
     }
